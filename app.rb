e6c8ea4e63de0a34c345c6702ee645bcd64744b9
Christopher Lubaway <asimplechris@gmail.com>
2014-07-15 14:18:04 -0700

    Adds more better notes.

--- app.rb
+++ app.rb
@@ -12,10 +12,11 @@ begin
 rescue LoadError
 end
 
+# Set the Database to be aware of current environment
 set :database, ENV['DATABASE_URL']
 ActiveRecord::Base.establish_connection(:adapter => 'postgresql')
 
-
+# Load All the ruby files in routes and models
 ['routes', 'models'].each do |directory_to_preload|
   Dir["#{directory_to_preload}/*.rb"].each do |file_to_load|
     require file_to_load
--- db/migrate/1_create_users.rb
+++ db/migrate/1_create_users.rb
@@ -1,3 +1,4 @@
+# I have to match the filename
 class CreateUsers < ActiveRecord::Migration
 
   def change
